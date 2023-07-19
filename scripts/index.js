642820059f036981dd714acf5230b39dfefa460f
Erik Aakre <erikaakre@hotmail.com>
2023-07-19 16:33:19 -0500

    Add open and close modal and render initial cards through js

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,63 @@ let initialCards = [
   },
 ];
 
+// Find modal form for editing profile in DOM
+let modalForm = document.querySelector(".modal__form");
+
+// Find open and close buttons for edit profile button
 let editButton = document.querySelector(".profile__edit-button");
-let modal = document.querySelector(".modal");
+let profileEditModal = document.querySelector("#profile-edit-modal");
 let modalCloseButton = document.querySelector(".modal__close");
 
+// find the profile elements in DOM
+let profileName = document.querySelector(".profile__title");
+let profileDescription = document.querySelector(".profile__description");
+
+// find the form fields in DOM
+let inputName = profileEditModal.querySelector(".form__input_name");
+let inputDescription = profileEditModal.querySelector(
+  ".form__input_description"
+);
+
+// open edit profile modal on click of edit button
 editButton.addEventListener("click", function () {
-  modal.classList.add("modal_opened");
+  profileEditModal.classList.add("modal_opened");
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
 });
 
+// close edit profile modal on click of close button
 modalCloseButton.addEventListener("click", function () {
-  modal.classList.remove("modal_opened");
+  profileEditModal.classList.remove("modal_opened");
+});
+
+// handle the form submission to save name and description
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  const newName = inputName.value;
+  const newDescription = inputDescription.value;
+
+  profileName.textContent = newName;
+  profileDescription.textContent = newDescription;
+}
+
+// Add functionality to Save button
+modalForm.addEventListener("click", handleProfileFormSubmit);
+
+// Get template for initial card setup
+let cardTemplate = document.querySelector("#cards__list-item").content;
+let cardsList = document.querySelector(".cards__list");
+let cardElement = cardTemplate
+  .querySelector(".cards__list-item")
+  .cloneNode(true);
+
+// Loop through cards to render on DOM
+initialCards.forEach((cardData) => {
+  let cardElement = cardTemplate
+    .querySelector(".cards__list-item")
+    .cloneNode(true);
+  cardElement.querySelector(".cards__image").src = cardData.link;
+  cardElement.querySelector(".cards__title").textContent = cardData.name;
+  cardsList.append(cardElement);
 });
