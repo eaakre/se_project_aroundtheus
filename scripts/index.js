f1afea9035f57543a107c68729c0738bc0dcdd6e
Erik Aakre <erikaakre@hotmail.com>
2023-07-20 15:18:21 -0500

    Fix submit button and mobile style

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Disney World",
     link: "https://images.unsplash.com/photo-1597466599360-3b9775841aec?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
@@ -25,63 +25,59 @@ let initialCards = [
   },
 ];
 
-// Find modal form for editing profile in DOM
-let modalForm = document.querySelector(".modal__form");
-
 // Find open and close buttons for edit profile button
-let editButton = document.querySelector(".profile__edit-button");
-let profileEditModal = document.querySelector("#profile-edit-modal");
-let modalCloseButton = document.querySelector(".modal__close");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = document.querySelector(".modal__close");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 
-// find the profile elements in DOM
-let profileName = document.querySelector(".profile__title");
-let profileDescription = document.querySelector(".profile__description");
-
-// find the form fields in DOM
-let inputName = profileEditModal.querySelector(".form__input_name");
-let inputDescription = profileEditModal.querySelector(
+// find the form fields in
+// Find modal form for editing profile in DOM
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const inputName = profileEditModal.querySelector(".form__input_name");
+const inputDescription = profileEditModal.querySelector(
   ".form__input_description"
 );
 
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
 // open edit profile modal on click of edit button
-editButton.addEventListener("click", function () {
+profileEditButton.addEventListener("click", function () {
   profileEditModal.classList.add("modal_opened");
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
 });
 
 // close edit profile modal on click of close button
-modalCloseButton.addEventListener("click", function () {
-  profileEditModal.classList.remove("modal_opened");
+profileEditCloseButton.addEventListener("click", () => {
+  closePopup();
 });
 
-// handle the form submission to save name and description
-function handleProfileFormSubmit(evt) {
+// save profile information on submit
+profileEditForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
-
-  const newName = inputName.value;
-  const newDescription = inputDescription.value;
-
-  profileName.textContent = newName;
-  profileDescription.textContent = newDescription;
-}
-
-// Add functionality to Save button
-modalForm.addEventListener("click", handleProfileFormSubmit);
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  closePopup();
+});
 
 // Get template for initial card setup
-let cardTemplate = document.querySelector("#cards__list-item").content;
-let cardsList = document.querySelector(".cards__list");
-let cardElement = cardTemplate
+const cardTemplate = document.querySelector("#cards__list-item").content;
+const cardsList = document.querySelector(".cards__list");
+const cardElement = cardTemplate
   .querySelector(".cards__list-item")
   .cloneNode(true);
 
 // Loop through cards to render on DOM
 initialCards.forEach((cardData) => {
-  let cardElement = cardTemplate
+  const cardElement = cardTemplate
     .querySelector(".cards__list-item")
     .cloneNode(true);
   cardElement.querySelector(".cards__image").src = cardData.link;
+  cardElement.querySelector(".cards__image").alt = cardData.name;
   cardElement.querySelector(".cards__title").textContent = cardData.name;
   cardsList.append(cardElement);
 });
